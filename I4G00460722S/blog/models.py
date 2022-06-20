7007c7b64afdd739c17176d3e6103864f4de3149
Amarachi Ajah     -     Software Developer | UX/UI Designer <anitaisbeta@gmail.com>
2022-06-20 23:57:57 +0100

    update

--- I4G00460722S/blog/models.py
+++ I4G00460722S/blog/models.py
@@ -1,13 +1,12 @@
-from turtle import title
-from django.db import models
 from django.conf import settings
+from django.db import models
 from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    authur = models.ForeignKey(get_user_model(), on_delete=models.DO.NOTHING)
+    author = models.ForeignKey(get_user_model(), on_delete=models.DO_NOTHING)
     created_date = models.DateTimeField()
     published_date = models.DateTimeField()
     
